81575a33ab9b53f738041e94f34d3917408b12a9
Malachi Coan <jmcoan4320@gmail.com>
2023-01-22 12:02:55 -0500

    Finished adding basic calculator functionality

--- src/Project1/Calculator.java
+++ src/Project1/Calculator.java
@@ -1,31 +1,23 @@
 package Project1;
 
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class Calculator {
-Scanner scnr = new Scanner(System.in);
+    Scanner scnr = new Scanner(System.in);
+
     public void calculate() {
         System.out.println("What is the first number? ");
         float n1 = Float.valueOf(scnr.nextLine());
         boolean end = false;
-        HashMap<String, String> operations = new HashMap<>();
-        operations.put("+", "add");
-        // Populate commands map
-        //operations.put("+", () -> add());
-        //operations.put('t', () -> System.out.println("Teleport"));
-        //operations.put('h', () -> System.out.println("Help"));
-        //operations.put('t', () -> System.out.println("Teleport"));
-//        ArrayList<String>operations = new ArrayList<>();
-//        operations.add("+");
-//        operations.add("-");
-//        operations.add("*");
-//        operations.add("/");
-
-
-        // Invoke some command
-        //String cmd = "+";
-        //operations.get(cmd).run();   // Prints "Teleport"
+        String go_again;
+        ArrayList<String> operations = new ArrayList<>();
+        operations.add("+");
+        operations.add("-");
+        operations.add("*");
+        operations.add("/");
 
         while (!end) {
             System.out.println("What operation do you want to use next? ");
@@ -34,37 +26,39 @@ Scanner scnr = new Scanner(System.in);
             float n2 = Float.valueOf(scnr.nextLine());
             //String calc_function = operations.get(symbol);
             float new_answer = calc_function(n1, n2, symbol);
-            System.out.println(n1 + " " + symbol + " " + n2 + "=" + new_answer);
-//            go_again = input("Do you want to continue adding to the equation? ")
-//
-//            if (go_again == 'n'):
-//
-//            end = True
-//            calculator()
-//
-//            elif(go_again == 'y'):
-//
-//            n1 = new_answer
-//
-//    else:
-//            print("Please type either 'n' or 'y'.")
-//        }
-//        print(logo)
-//        calculator()
-//    }
+            System.out.println(n1 + " " + symbol + " " + n2 + " = " + new_answer);
+            System.out.println("Do you want to continue? Type 'y' or 'n'");
+            go_again = scnr.nextLine();
 
+            if (go_again.equals("n")) {
+                end = true;
+                calculate();
+            } else if (go_again.equals("y")) {
 
-
+                n1 = new_answer;
+            } else {
+                System.out.println("Please type either 'n' or 'y'.");
+            }
 
         }
     }
 
     private float calc_function(float n1, float n2, String symbol) {
-            float answer = 0;
-            if (symbol.equals("+")) {
-                answer = n1 + n2;
-            }
-            return answer;
+        float answer = 0;
+        if (symbol.equals("+")) {
+            answer = n1 + n2;
+        }
+        if (symbol.equals("*")) {
+            answer = n1 * n2;
+        }
 
+        if (symbol.equals("-")) {
+            answer = n1 - n2;
+        }
+
+        if (symbol.equals("/")) {
+            answer = n1 / n2;
+        }
+        return answer;
     }
 }
--- src/Project1/Main.java
+++ src/Project1/Main.java
@@ -3,11 +3,9 @@ package Project1;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         Calculator calc = new Calculator();
-
         calc.calculate();
-
 
     }
 }
